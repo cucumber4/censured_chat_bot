bffea17dba2960718f971072804bfa155de88837
cucumber4 <gogoreferenc@gmail.com>
2023-06-02 14:18:04 +0600

    move files

---
+++ config.py
@@ -0,0 +1,6 @@
+main_token='f5f0f74cba01e73be8b6357422729051daa12c470f624586ced8e4bfee2275d24cc7b6a1355e71764ea75'
+main_token2='dc53f2c7a3151f665314db1e18df7bf9623d0ea3cc04d58e9172853b645c09e3c79a9e5e34df4ac70e1f7'
+
+f = open('huh.txt' , 'a+')
+f.write('configuration was used ')
+f.close()
