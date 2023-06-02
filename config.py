bc67464b6dbc8489e1a2205b0ae37e8071472f4b
cucumber4 <gogoreferenc@gmail.com>
2023-06-02 14:22:13 +0600

    rename

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ main_token='f5f0f74cba01e73be8b6357422729051daa12c470f624586ced8e4bfee2275d24cc7
 main_token2='dc53f2c7a3151f665314db1e18df7bf9623d0ea3cc04d58e9172853b645c09e3c79a9e5e34df4ac70e1f7'
 
 f = open('huh.txt' , 'a+')
-f.write('configuration was used ')
+f.write('configuration was used \n')
 f.close()
